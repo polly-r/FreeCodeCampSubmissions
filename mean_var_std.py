bf08702b981e6fd1a1a0c5c9b8fc20945f338166
Polly <u18066365@tuks.co.za>
2024-08-22 11:56:14 +0200

    Submission 1

---
+++ mean_var_std.py
@@ -0,0 +1,29 @@
+import numpy as np
+
+def calculate(list):
+    # Error code if input is not 9 digits long.
+    if len(list) != 9:
+        raise ValueError("List must contain nine numbers.")
+
+    # Convert the list into a 3x3 Numpy array
+    matrix = np.array(list).reshape(3, 3)
+    
+     # Calculate the required statistics
+    mean = [matrix.mean(axis=0).tolist(), matrix.mean(axis=1).tolist(), matrix.mean().tolist()]
+    variance = [matrix.var(axis=0).tolist(), matrix.var(axis=1).tolist(), matrix.var().tolist()]
+    std_dev = [matrix.std(axis=0).tolist(), matrix.std(axis=1).tolist(), matrix.std().tolist()]
+    max_val = [matrix.max(axis=0).tolist(), matrix.max(axis=1).tolist(), matrix.max().tolist()]
+    min_val = [matrix.min(axis=0).tolist(), matrix.min(axis=1).tolist(), matrix.min().tolist()]
+    sum_val = [matrix.sum(axis=0).tolist(), matrix.sum(axis=1).tolist(), matrix.sum().tolist()]
+    
+    # Store the calculations in a dictionary
+    calculations = {
+        'mean': mean,
+        'variance': variance,
+        'standard deviation': std_dev,
+        'max': max_val,
+        'min': min_val,
+        'sum': sum_val
+    }
+
+    return calculations
